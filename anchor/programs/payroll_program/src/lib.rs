d72e14a26b84995e0a66211a3cf6f8dbc680e648
miri-10 <misanpokharel3@gmail.com>
2026-02-17 10:17:27 +0545

    rust program

--- anchor/programs/payroll_program/src/lib.rs
+++ anchor/programs/payroll_program/src/lib.rs
@@ -1,16 +1,39 @@
+#![allow(unexpected_cfgs)]
+
 use anchor_lang::prelude::*;
 
-declare_id!("A9fnM3skS5kbECt2isFV2EGvS4D7hnsMjnqi2YBwaeed");
+pub mod errors;
+pub mod instructions;
+pub mod states;
+
+use instructions::*;
+
+declare_id!("Gig5DYbHVv1xtLUmEA6AR5syACPCkLw1PddXwcg6Sen8");
 
 #[program]
-pub mod hello_world {
+pub mod payroll_program {
     use super::*;
 
-    pub fn hello(_ctx: Context<Hello>) -> Result<()> {
-        msg!("Hello, World!");
-        Ok(())
+    pub fn create_org(ctx: Context<CreateOrgCtx>, name: String) -> Result<()> {
+        instructions::create_org(ctx, name)
+    }
+
+    pub fn add_worker(ctx: Context<AddWorkerCtx>, salary: u64) -> Result<()> {
+        instructions::add_worker(ctx, salary)
+    }
+
+    pub fn fund_treasury(ctx: Context<FundTreasuryCtx>, amount: u64) -> Result<()> {
+        instructions::fund_treasury(ctx, amount)
+    }
+
+    pub fn process_payroll<'info>(
+        ctx: Context<'_, '_, 'info, 'info, ProcessPayrollCtx<'info>>,
+        cycle_timestamp: u64,
+    ) -> Result<()> {
+        instructions::process_payroll(ctx, cycle_timestamp)
+    }
+
+    pub fn withdraw(ctx: Context<WithdrawCtx>, amount: u64) -> Result<()> {
+        instructions::withdraw(ctx, amount)
     }
 }
-
-#[derive(Accounts)]
-pub struct Hello {}
