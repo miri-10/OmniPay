6da3622ad7b57f17c5d204f1dbab07a14c5682c7
miri-10 <misanpokharel3@gmail.com>
2026-02-24 04:07:07 +0545

    id

--- anchor/programs/payroll_program/src/lib.rs
+++ anchor/programs/payroll_program/src/lib.rs
@@ -8,7 +8,7 @@ pub mod states;
 
 use instructions::*;
 
-declare_id!("Gig5DYbHVv1xtLUmEA6AR5syACPCkLw1PddXwcg6Sen8");
+declare_id!("5igZugtxpgEM2J5DA5cyUdWW857FY7TC6hKeA8bLHM88");
 
 #[program]
 pub mod payroll_program {
